d3395bf087a6b120cecac48e6da3587c195e4084
Aleksandra Rylova <0sasha0rul18@mail.ru>
2024-05-20 14:58:21 +0500

    [classtext] Add build.bat and improve gitignore

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001 > log
+del log
+
+set BUILD_TYPE="Visual Studio 17 2022"
+set BUILD_FOLDER=build_vs
+set SOURCE_FOLDER=.
+
+if not exist %BUILD_FOLDER% mkdir %BUILD_FOLDER%
+cd %BUILD_FOLDER%
+
+cmake -G %BUILD_TYPE% ..\%SOURCE_FOLDER%
+cmake --build .
